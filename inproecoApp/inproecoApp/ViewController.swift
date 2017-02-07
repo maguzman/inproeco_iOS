11fb87108a19bdb614f221c57d5272ee31e1ba1e
Miguel Angel Guzmán Espinosa <mguzman@hastengroup.com>
2017-02-07 14:23:32 +0100

    Ficheros de creación del proyecto

---
+++ inproecoApp/inproecoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  inproecoApp
+//
+//  Created by Miguel Angel Guzmán Espinosa on 7/2/17.
+//  Copyright © 2017 Ingeniería y Proyectos Ecológicos, S.L. (INPROECO). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
